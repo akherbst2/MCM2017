2562356bbe4b0c051af98d3038c6b5173da99ca0
Alyssa Herbst <alyssa2@vt.edu>
2017-01-19 20:07:34 -0500

    Added a Road class and barebones functions

---
+++ src/CarGenerator.java
@@ -0,0 +1,37 @@
+/**
+ * Constructor-- read from scanner for data I need
+ * Maybe 10 numbers?
+ * Time intervals, new car per lane, number of lanes, random seed
+ *
+ * carGenerator take a road in constructor
+ * calling generate, will add the cars onto the road
+ *
+ *
+ * @author Alyssa Herbst
+ * created on 1/19/2017.
+ */
+
+import java.util.*;
+import java.io.*;
+
+import static java.lang.Math.*;
+
+public class CarGenerator {
+    //The interval of time at which we are at currently.  Increases by 1 each time.
+    private double timeInterval;
+    private Road road;
+
+
+
+    public CarGenerator(Road road, double timeInterval) {
+
+
+
+    }
+
+    //Adds cars where appropriate onto the road
+    public void generate() {
+
+
+    }
+}
---
+++ src/Road.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * TODO
+ * @author Alyssa Herbst
+ * created on 1/19/2017.
+ */
+
+import java.util.*;
+import java.io.*;
+
+import static java.lang.Math.*;
+
+public class Road {
+
+}
