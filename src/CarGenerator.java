ef1e3e4e014b9aa0668b66f564f34d57fb4a5f65
Alyssa Herbst <alyssa2@vt.edu>
2017-01-19 20:47:18 -0500

    Added CarGenerator Methods.

--- src/CarGenerator.java
+++ src/CarGenerator.java
@@ -18,20 +18,38 @@ import static java.lang.Math.*;
 
 public class CarGenerator {
     //The interval of time at which we are at currently.  Increases by 1 each time.
-    private double timeInterval;
-    private Road road;
+    private double currentTime;
+    private double timeTickSize;
+    private List<Road> roads;//  Gets the frequency from roads
 
 
+    public CarGenerator(List<Road> roads, double currentTime, double timeTickSize) {
+        this.roads = roads;
+        this.currentTime = currentTime;
+        this.timeTickSize = timeTickSize;
+    }
 
-    public CarGenerator(Road road, double timeInterval) {
-
-
-
+    private void advanceTime() {
+        currentTime += timeTickSize;
+        for(Road road:roads) {
+            road.advanceTimeBy(timeTickSize);
+        }
     }
 
     //Adds cars where appropriate onto the road
     public void generate() {
+        advanceTime();
 
+        for(Road road:roads) {
+            //The number of seconds between each car pass
+            double timeLapsePerCar = road.getTimeLapsePerCar();
+            //if the time that the car should have passed has passed sometime within the last time interval
+            //then send out a car at that time.
 
+            if(road.getTimeSinceLastCar() >= timeLapsePerCar) {
+                road.addCar(new Car());
+
+            }
+        }
     }
 }
--- src/Road.java
+++ src/Road.java
@@ -11,5 +11,36 @@ import java.io.*;
 import static java.lang.Math.*;
 
 public class Road {
+    private int AverageDailyTrafficCounts;
+    private double timeSinceLastCar;
+    private List<Car> cars;
 
+    public Road(int averageDailyTrafficCounts) {
+        AverageDailyTrafficCounts = averageDailyTrafficCounts;
+    }
+
+    /**
+     * Will alter this with standard distribution curve later.
+     * @return
+     */
+    public double getTimeLapsePerCar() {
+        return  (this.getAverageDailyTrafficCounts()) / (24.0 * 60 * 60);
+    }
+
+    public void addCar(Car car) {
+        this.cars.add(car);
+        this.timeSinceLastCar -= this.getTimeLapsePerCar();
+    }
+
+    public int getAverageDailyTrafficCounts() {
+        return AverageDailyTrafficCounts;
+    }
+
+    public double getTimeSinceLastCar() {
+        return timeSinceLastCar;
+    }
+
+    public void advanceTimeBy(double time) {
+        timeSinceLastCar += time;
+    }
 }
