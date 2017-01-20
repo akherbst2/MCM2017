f714fdf1aea071f57c986ffe54ad22653f653883
peter707 <peter707@vt.edu>
2017-01-19 20:54:07 -0500

    Initial skeleton code. Cars drive at constant speeds in straight lines.

---
+++ Car.java
@@ -0,0 +1,31 @@
+package MCM;
+
+public class Car {
+	double xPos;
+	double yPos;
+	double xVel;
+	double yVel;
+	boolean laneChange;
+	double accPotential;
+	double breakPotential;
+	double aggressiveness;
+	Road road;
+	int lane;
+	public Car(Road road, int lane, double laneWidth, double initialSpeed){
+		this.road = road;
+		this.lane = lane;
+		xPos = lane * laneWidth + .5 * laneWidth;
+		yPos = 0;
+		yVel = initialSpeed;
+		xVel = 0;
+	}
+	
+	
+	public void move(double timeInterval){
+		xPos += xVel * timeInterval;
+		yPos += yVel * timeInterval;
+	}
+	public String toString(){
+		return "X: " + xPos + " Y: " + yPos + " Lane: " + lane;
+	}
+}
