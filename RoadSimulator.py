9558073ac088b0ec3fd6cf9142d022e1e823b9c6
Ariana Herbst <herbstariana@gmail.com>
2017-01-20 15:57:31 -0500

    Add files via upload

--- RoadSimulator.py
+++ RoadSimulator.py
@@ -11,7 +11,8 @@ except ImportError:
 Window_Width=1000
 Window_Height=700
 laneWidth_ft = 12
-lanes = 18
+buffer = 100
+lanes = 8
 scale = 1
 t = 0
 deltaT = .1
@@ -19,10 +20,12 @@ deltaT = .1
 class Vehicle:
     def __init__(self, id, xloc, yloc, xsize, ysize):
         self.id = id
-        self.rect = canvas.create_rectangle((xloc - (xsize / 2.0)) * scale , 
+        self.rect = canvas.create_rectangle(buffer + (xloc - (xsize / 2.0)) * scale , 
                                             Window_Height -( yloc + ysize) * scale,
-                                             (xloc + (xsize / 2.0)) * scale, Window_Height - yloc*scale,
+                                             buffer + (xloc + (xsize / 2.0)) * scale, Window_Height - yloc*scale,
                                               fill='green')
+        #print(scale)
+        
         self.xloc = xloc
         self.yloc = yloc
         self.xsize = xsize
@@ -44,15 +47,15 @@ class Vehicle:
         
 class Road:
     def __init__(self):
-        self.rect = canvas.create_rectangle(0, 0, 50 + lanes * laneWidth_ft * scale, Window_Height, fill='#111')
+        self.rect = canvas.create_rectangle(buffer, 0,  buffer + lanes * laneWidth_ft * scale, Window_Height, fill='#111')
         for i in range(1, lanes):
-            canvas.create_line( i * (laneWidth_ft * scale), 0,  i * (laneWidth_ft * scale), Window_Height, fill="yellow", dash=(6,5)) 
+            canvas.create_line( buffer + i * (laneWidth_ft * scale), 0,  buffer + i * (laneWidth_ft * scale), Window_Height, fill="yellow", dash=(6,5)) 
         
         
 def readFile():
      f = open("CarData2.csv")
      rows = csv.reader(f)
-     next(rows, None)
+     #next(rows, None)
      return rows
  
 def exitWindow(root):
@@ -60,6 +63,12 @@ def exitWindow(root):
         
 def main():
     File = readFile()
+    sizes = File.next()
+    global scale
+    global lanes
+    scale = Window_Height * 1.0 / float(sizes[0])
+    lanes = int(sizes[1])
+    Window_Width = lanes * laneWidth_ft * scale + buffer * 2
     root = Tk()
     global canvas
     canvas = Canvas(root, bg="#632", width=Window_Width, height=Window_Height)
@@ -67,9 +76,14 @@ def main():
     road = Road()
     vehicle_Dict = {}
     t = 0
-    for line in File:   
+    
+    """start animation"""
+    for line in File:
+        if line[0] == "stop":
+            del vehicle_Dict[line[1]]
+            continue
         if not t == line[0]:
-            canvas.after(100)
+            canvas.after(50)
             t = line[0]
         vehicle = vehicle_Dict.get(line[1])
         if vehicle == None:
