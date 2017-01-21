a8c6c27b8d37b60cb5655cbf9f88632e1fdabf74
Alyssa Herbst <alyssa2@vt.edu>
2017-01-20 21:19:36 -0500

    Makes Velocity and Accelleration Decisions

--- src/Car.java
+++ src/Car.java
@@ -9,6 +9,7 @@ public class Car {
 	double yAccel;
 	//maximum accel or decel in ft/s
 	static final double ACCEL_LIMIT = 15;
+	double INITIAL_SPEED;
 	// double accPotential;
 	// double breakPotential;
 	// double aggressiveness;
@@ -27,6 +28,7 @@ public class Car {
 		yPos = 0;
 		id = carCount;
 		yVel = initialSpeed;
+		this.INITIAL_SPEED = initialSpeed;
 		xVel = 0;
 		this.laneWidth = laneWidth;
 		xSize = 6;
@@ -48,11 +50,72 @@ public class Car {
 		return false;
 	}
 
+	double getMinAccelNeededToNotCrash(Car other) {
+		double vRel = other.yVel - this.yVel;
+		double carDist = other.yPos - this.yPos - ySize;
+
+		return (Math.pow(vRel, 2))/(2*(carDist)) - other.yAccel;
+	}
+
+	Car getCarInFront() {
+		Car closestCar = null;
+		double closestDist = Double.MAX_VALUE;
+		for(Car other:this.road.carList) {
+			if(other.lane == this.lane) {
+				//check the yDist.
+				//if other car is in front
+				if(other.yPos > this.yPos) {
+					if((other.yPos - this.yPos) < closestDist) {
+						closestCar = other;
+						closestDist = other.yPos - this.yPos;
+					}
+				}
+			}
+		}
+
+		return closestCar;
+	}
+
+	//TODO peter do this :)
+	public boolean attemptLaneChange() {
+		return false;
+	}
+
+	public void makeVelocityAndAccelerationDecisions(double timeInterval) {
+		double followDistance = (yVel / 10) * ySize;
+		Car closest = getCarInFront();
+
+		if((closest != null)&&((closest.yPos - this.yPos) < followDistance)) {
+			if(isExpectedCrash(closest)) {
+				attemptLaneChange();
+			}
+			else {
+				//Slows down at an acceleration that is somewhere between the minimum deceleration needed
+				//to not crash, and the max acceleration of the car
+				double minAccel = getMinAccelNeededToNotCrash(closest);
+				yAccel = -1 * ((minAccel + ACCEL_LIMIT) / 2);
+			}
+		}
+		else if(yVel < INITIAL_SPEED) {
+			//Speeds up to ideal speed if it's possible
+			if((INITIAL_SPEED - yVel)*(1 / timeInterval) < ACCEL_LIMIT) {
+				yVel = INITIAL_SPEED;
+			}
+			//otherwise, get as close to the ideal speed as we can
+			else {
+				yVel += (ACCEL_LIMIT * timeInterval);
+			}
+		}
+	}
+
 
 	public void move(double timeInterval) {
 
 		//make acceleration decisions
+		makeVelocityAndAccelerationDecisions(timeInterval);
 
+		xVel += xAccel * timeInterval;
+		yVel += yAccel * timeInterval;
 
 		xPos += xVel * timeInterval;
 		yPos += yVel * timeInterval;
