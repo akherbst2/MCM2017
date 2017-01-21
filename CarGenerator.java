98f45810d72d44976dbd53e434370be82b63314f
peter707 <peter707@vt.edu>
2017-01-20 20:22:26 -0500

    Fixed bug involving car spawns

---
+++ CarGenerator.java
@@ -0,0 +1,48 @@
+package MCM;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CarGenerator {
+	double freq;
+    double currentTime;
+    double timeTickSize;
+	Road road;
+	int carCount = 0;
+	double initialSpeedOfCar;
+	double[] timeLastCar;
+	public CarGenerator(Scanner input, Road road, int numberOfLanes, double freq, double timeStep){
+		this.freq = freq;
+		this.road = road;
+		this.timeTickSize = timeStep;
+		initialSpeedOfCar = 88;
+		currentTime = 0;
+		timeLastCar = new double[numberOfLanes];
+		Arrays.fill(timeLastCar, currentTime-1000);
+	}
+	
+	public void advanceTime() {
+        currentTime += timeTickSize;
+    }
+
+	
+	
+	public Car getCar(){
+		int laneId = -1;
+		double maxTimeDiff = -1;
+		for(int a = 0; a < timeLastCar.length; a++){
+			if(maxTimeDiff < currentTime-timeLastCar[a]){
+				maxTimeDiff = currentTime-timeLastCar[a];
+				laneId = a;
+			}
+		}
+		
+		if(Math.random() < freq * timeTickSize && road.numberOfCars() < 500){
+			carCount++;
+			timeLastCar[laneId] = currentTime;
+			return new Car(road, laneId, road.roadWidth, 88, carCount);
+		}
+		return null;
+	}
+
+}
